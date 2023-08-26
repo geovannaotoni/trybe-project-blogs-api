bd9c09dacb489a7d44ed3752c8a50c468bbb8659
Geovanna Otoni <geovannaotoni@gmail.com>
2023-08-26 11:27:51 -0300

    feat11: cria model PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,47 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      field: 'post_id',
+      references: {
+        model: 'blog_posts',
+        id: 'id',
+      }
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      field: 'category_id',
+      references: {
+        model: 'categories',
+        id: 'id',
+      }
+    }
+  },
+  {
+    timestamps: false,
+    tableName: 'posts_categories',
+    underscored: true, 
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  }
+
+  return PostCategory;
+};
