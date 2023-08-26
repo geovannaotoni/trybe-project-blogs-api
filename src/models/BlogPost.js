fd1bb6d8a1cba9b4d657696e38aab941f5eb277f
Geovanna Otoni <geovannaotoni@gmail.com>
2023-08-26 11:11:58 -0300

    feat10: cria model BlogPost

---
+++ src/models/BlogPost.js
@@ -0,0 +1,45 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      field: 'user_id',
+      foreignKey: true,
+    },
+    published: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updated: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'blog_posts',
+    underscored: true, 
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId', as: 'user'
+    });
+  }
+
+  return BlogPost;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -29,5 +29,11 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true, 
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId', as: 'blogPosts'
+    })
+  }
+
   return User;
 };
